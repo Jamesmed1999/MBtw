282cde54b9e181ea13628f2dc173d6a7991c1a60
Jamesmed1999 <Jamesmed1999@gmail.com>
2022-01-28 19:51:46 -0500

    transfer commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Charpage from './Pages/Charpage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Melty Tech</h1>
+      <Charpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Charpage.js
@@ -0,0 +1,15 @@
+import './CharPage.css'
+function Charpage()
+{
+    let j = 12;
+    console.log(j)
+
+    return(
+        <div className="ST">
+        <h2>Shiki Tono</h2>
+        <p>Twitter go here</p>
+        </div>
+        
+    );
+}
+export default Charpage;
